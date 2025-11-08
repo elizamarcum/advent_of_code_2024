eb6e119c745d2c5c9ee1877a57eb910e03b0b499
Eliza Brock Marcum <eliza@elizamarcum.com>
2025-11-07 21:23:32 -0700

    Add DayN spec for use as template

--- lib/dayN.rb
+++ lib/dayN.rb
@@ -3,8 +3,10 @@ class DayN
   end
 
   def self.part1(input)
+    "TBD"
   end
 
   def self.part2(input)
+    "TBD"
   end
 end
---
+++ spec/dayN_spec.rb
@@ -0,0 +1,26 @@
+require_relative 'spec_helper'
+
+# http://adventofcode.com/2024/day/2
+
+describe DayN do
+  let(:input) {
+    <<~STRING
+    STRING
+  }
+
+  describe "part1" do
+    it "overall scenario" do
+      expected = "TBD"
+      actual = DayN.part1(input)
+      _(actual).must_equal(expected)
+    end
+  end
+
+  describe "part2" do
+    it "overall scenario" do
+      expected = "TBD"
+      actual = DayN.part2(input)
+      _(actual).must_equal(expected)
+    end
+  end
+end
