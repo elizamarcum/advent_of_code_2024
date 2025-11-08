f5af8c9b187d75e8a3877b696ec1fd5b23ec4d37
Eliza Brock Marcum <eliza@elizamarcum.com>
2025-11-07 21:12:30 -0700

    Day 2, Part 2

--- lib/day02.rb
+++ lib/day02.rb
@@ -9,11 +9,22 @@ class Day2
     self.numbers_all_increase_safely?(numbers) or self.numbers_all_decrease_safely?(numbers)
   end
 
+  def self.report_safe_if_dampened?(numbers)
+    return true if self.report_safe?(numbers)
+    numbers.count.times do |i|
+      numbers_without_item_at_i = Array.new(numbers)
+      numbers_without_item_at_i.delete_at(i)
+      return true if self.report_safe?(numbers_without_item_at_i)
+    end
+    false
+  end
+
   def self.part1(input)
     self.parse_input(input).count{|report| self.report_safe?(report) }
   end
 
   def self.part2(input)
+    self.parse_input(input).count{|report| self.report_safe_if_dampened?(report) }
   end
 
   private
--- spec/day02_spec.rb
+++ spec/day02_spec.rb
@@ -71,6 +71,51 @@ describe Day2 do
 
   describe "part2" do
     it "overall scenario" do
+      expected = 4
+      actual = Day2.part2(input)
+      _(actual).must_equal(expected)
+    end
+  end
+
+  describe "report_safe_if_dampened?" do
+    it "is safe if levels are all increasing by 1, 2, or 3" do
+      input = %w(7 6 4 2 1).map(&:to_i)
+      assert Day2.report_safe_if_dampened?(input)
+    end
+
+    it "is unsafe if removing levels still has the increase above 3" do
+      input = %w(1 2 7 8 9).map(&:to_i)
+      refute Day2.report_safe_if_dampened?(input)
+    end
+
+    it "is safe if removing levels no longer has the increase above 3" do
+      input = %w(1 2 3 4 10).map(&:to_i)
+      assert Day2.report_safe_if_dampened?(input)
+    end
+
+    it "is unsafe if removing levels still has the decrease above 3" do
+      input = %w(9 7 6 2 1).map(&:to_i)
+      refute Day2.report_safe_if_dampened?(input)
+    end
+
+    it "is safe if removing levels no longer has the decrease above 3" do
+      input = %w(10 9 8 4).map(&:to_i)
+      assert Day2.report_safe_if_dampened?(input)
+    end
+
+    it "is safe if level increase then decrease disappears with level removal" do
+      input = %w(1 3 2 4 5).map(&:to_i)
+      assert Day2.report_safe_if_dampened?(input)
+    end
+
+    it "is safe if the only issue is a single duplicate level" do
+      input = %w(8 6 4 4 1).map(&:to_i)
+      assert Day2.report_safe_if_dampened?(input)
+    end
+
+    it "is safe if levels are all decreasing by 1, 2, or 3" do
+      input = %w(1 3 6 7 9).map(&:to_i)
+      assert Day2.report_safe_if_dampened?(input)
     end
   end
 end
