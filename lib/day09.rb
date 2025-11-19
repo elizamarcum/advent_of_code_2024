074b2c31f6e0cd1c77daf2cf408a7feeaaccf5c3
Eliza Brock Marcum <eliza@elizamarcum.com>
2025-11-18 16:56:16 -0800

    Day 9, Part 1 refactored to support Part 2

--- lib/day09.rb
+++ lib/day09.rb
@@ -1,6 +1,6 @@
 class Day9
   def self.part1(input)
-    fs = Day9::FileSystem.from_diskmap(input)
+    fs = Day9::BlockFileSystem.new(diskmap: input)
     fs.compact!
     fs.checksum
   end
@@ -12,27 +12,65 @@ class Day9
   class FileSystem
     FREESPACE = '.'
 
-    def initialize(input)
-      @blocks = input.is_a?(Array) ? input : input.split(//).map{ |char| char.ord.between?('0'.ord, '9'.ord) ? char.to_i : char }
+    def initialize(string_input = nil, diskmap: nil)
+      units = string_input ? self.class.parse_from_string(string_input) : self.class.parse_diskmap(diskmap)
+      set_units(units)
     end
 
     def all_free_space_contiguous?
-      first_free_space = @blocks.index(FREESPACE)
-      last_file_space = @blocks.rindex{ |e| e != FREESPACE }
+      first_free_space = blocks.index(FREESPACE)
+      last_file_space = blocks.rindex{ |e| e != FREESPACE }
       !first_free_space or first_free_space > last_file_space
     end
 
     def checksum
-      @blocks.map.with_index do |filenumber, index|
+      blocks.map.with_index do |filenumber, index|
         (filenumber == FREESPACE) ? 0 : filenumber * index
       end.sum
     end
 
+    def to_s
+      blocks.join()
+    end
+
+    private
+
+    # Blocks and units will not always be the same concept in child classes.
+    # Some child classes are expected to have units that are multiple blocks long,
+    # and thus will have to implement something different for this..
+    def blocks
+      @units
+    end
+
+    def set_units(units)
+      @units = units
+    end
+
+    def self.parse_diskmap(diskmap)
+      instructions = diskmap.split(//).map(&:to_i)
+      file_number = -1
+      blocks = []
+
+      instructions.each_slice(2) do |slice|
+        file_length, freespace_length = slice
+        file_number += 1
+        file_length.times{ blocks << file_number }
+        freespace_length.times{ blocks << FREESPACE } if freespace_length
+      end
+      blocks
+    end
+
+    def self.parse_from_string(string_input)
+      string_input.split(//).map{ |char| char.ord.between?('0'.ord, '9'.ord) ? char.to_i : char  }
+    end
+  end
+
+  class BlockFileSystem < FileSystem
     def compact!(limit: nil)
       compaction_count = 0
       compacted = []
       free_space_to_add = 0
-      remaining = @blocks
+      remaining = @units
       until remaining.empty? or (limit and compaction_count >= limit)
         # Confirm that there are filled blocks remaining
         final_occupied_block = remaining.rindex{|e| e != FREESPACE}
@@ -47,7 +85,7 @@ class Day9
         break if remaining.empty?
 
         # Pull all of the filled space off the front of the list and tuck it into `compacted`
-        first_of_freespace_at_beginning = remaining.index(FREESPACE) or -1
+        first_of_freespace_at_beginning = remaining.index(FREESPACE)
         blocks_at_beginning = remaining.slice!(0...first_of_freespace_at_beginning)
         compacted.push(*blocks_at_beginning)
 
@@ -63,30 +101,9 @@ class Day9
         # Increment loop counter
         compaction_count += 1
       end
-      # Update @blocks to contain the compacted blocks along with anything remaining
+      # Update @units to contain the compacted blocks along with anything remaining
       # in the working list and all the free space
-      @blocks = compacted + remaining + Array.new(free_space_to_add){ |i| FREESPACE }
-    end
-
-    def compact_once!
-      compact!(limit: 1)
-    end
-
-    def to_s
-      @blocks.join()
-    end
-
-    def self.from_diskmap(str)
-      instructions = str.split(//).map(&:to_i)
-      file_number = -1
-      blocks = []
-      instructions.each_slice(2) do |slice|
-        file_length, freespace_length = slice
-        file_number += 1
-        file_length.times{ blocks << file_number }
-        freespace_length.times{ blocks << FREESPACE } if freespace_length
-      end
-      FileSystem.new(blocks)
+      set_units(compacted + remaining + Array.new(free_space_to_add){ |i| FREESPACE })
     end
   end
 end
--- spec/day09_spec.rb
+++ spec/day09_spec.rb
@@ -15,103 +15,128 @@ describe Day9 do
 
   describe "part2" do
     it "overall scenario" do
-      expected = "TBD"
+      skip
+      expected = 2858
       actual = Day9.part2(overall_scenario_input)
       _(actual).must_equal(expected)
     end
   end
 
   describe Day9::FileSystem do
-    let(:expected_output_1){ '0..111....22222' }
-    let(:expected_output_2){ '02.111....2222.' }
-    let(:expected_output_3){ '022111....222..' }
-    let(:expected_output_4){ '0221112...22...' }
-    let(:expected_output_5){ '02211122..2....' }
-    let(:expected_output_6){ '022111222......' }
-
-    describe ".from_diskmap" do
+    describe "#new with diskmap" do
       it 'parses the diskmap into file block positions' do
-        actual = Day9::FileSystem.from_diskmap('2333133121414131402')
+        actual = Day9::BlockFileSystem.new(nil, diskmap: '2333133121414131402').to_s
         expected = '00...111...2...333.44.5555.6666.777.888899'
-        _(actual.to_s).must_equal(expected)
+        _(actual).must_equal(expected)
+      end
+    end
+
+    describe "#new from string" do
+      it 'results in a BlockFileSystem that outputs the same as its to_s with a small example' do
+        expected = '00...111'
+        actual = Day9::BlockFileSystem.new(expected).to_s
+        _(actual).must_equal(expected)
+      end
+
+      it 'results in a BlockFileSystem that outputs the same as its to_s with a longer example' do
+        expected = '00...111...2...333.44.5555.6666.777.888899'
+        actual = Day9::BlockFileSystem.new(expected).to_s
+        _(actual).must_equal(expected)
+      end
+
+      it 'can handle a file that is split across multiple areas' do
+        expected = '9900...111...2...333.44.5555.6666.777.888899'
+        actual = Day9::BlockFileSystem.new(expected).to_s
+        _(actual).must_equal(expected)
       end
     end
 
     describe "#all_free_space_contiguous?" do
       it 'returns true when there is no free space' do
-        assert Day9::FileSystem.new('001122').all_free_space_contiguous?
+        assert Day9::BlockFileSystem.new('001122').all_free_space_contiguous?
       end
       it 'returns true when there is no gap in the free space' do
-        assert Day9::FileSystem.new(expected_output_6).all_free_space_contiguous?
+        assert Day9::BlockFileSystem.new('022111222......').all_free_space_contiguous?
       end
       it 'returns false when there are gaps in the free space' do
-        refute Day9::FileSystem.new(expected_output_5).all_free_space_contiguous?
+        refute Day9::BlockFileSystem.new('02211122..2....').all_free_space_contiguous?
       end
       it 'returns false if the freespace is all at the beginning' do
-        refute Day9::FileSystem.new(expected_output_6.reverse).all_free_space_contiguous?
+        refute Day9::BlockFileSystem.new('...02211122').all_free_space_contiguous?
       end
     end
 
     describe "#checksum" do
       it 'sums the values of block position x fileID' do
-        filesystem = Day9::FileSystem.new('0099811188827773336446555566..............')
+        filesystem = Day9::BlockFileSystem.new('0099811188827773336446555566..............')
         expected = 1928
         _(filesystem.checksum).must_equal(expected)
       end
     end
 
-    describe "#compact! moves file blocks one at a time from the end of the disk to the leftmost free space block until all free space is contiguous" do
-      it 'compacts the short example' do
-        fs = Day9::FileSystem.new(expected_output_1)
-        fs.compact!
-        _(fs.to_s).must_equal expected_output_6
-      end
-
-      it 'compacts the long example' do
-        fs = Day9::FileSystem.from_diskmap(overall_scenario_input)
-        expected = '0099811188827773336446555566..............'
-        fs.compact!
-        _(fs.to_s).must_equal expected
-      end
-    end
-
-    describe "#compact_once! moves file blocks one at a time from the end of the disk to the leftmost free space block" do
-      it 'moves from state 1 to state 2' do
-        fs = Day9::FileSystem.new(expected_output_1)
-        fs.compact_once!
-        _(fs.to_s).must_equal expected_output_2
-      end
-
-      it 'moves from state 2 to state 3' do
-        fs = Day9::FileSystem.new(expected_output_2)
-        fs.compact_once!
-        _(fs.to_s).must_equal expected_output_3
-      end
-
-      it 'moves from state 3 to state 4' do
-        fs = Day9::FileSystem.new(expected_output_3)
-        fs.compact_once!
-        _(fs.to_s).must_equal expected_output_4
-      end
-
-      it 'moves from state 4 to state 5' do
-        fs = Day9::FileSystem.new(expected_output_4)
-        fs.compact_once!
-        _(fs.to_s).must_equal expected_output_5
-      end
-
-      it 'moves from state 5 to state 6' do
-        fs = Day9::FileSystem.new(expected_output_5)
-        fs.compact_once!
-        _(fs.to_s).must_equal expected_output_6
-      end
-    end
-
     describe "#to_s" do
       it 'outputs a string representation of the file block positions' do
-        actual = Day9::FileSystem.new('00...111...2...333.44.5555.6666.777.888899')
+        actual = Day9::BlockFileSystem.new('00...111...2...333.44.5555.6666.777.888899')
         expected = '00...111...2...333.44.5555.6666.777.888899'
         _(actual.to_s).must_equal(expected)
+      end
+    end
+  end
+
+  describe Day9::BlockFileSystem do
+    let(:input){ '0..111....22222' }
+    let(:expected_output_1){ '02.111....2222.' }
+    let(:expected_output_2){ '022111....222..' }
+    let(:expected_output_3){ '0221112...22...' }
+    let(:expected_output_4){ '02211122..2....' }
+    let(:expected_output_5){ '022111222......' }
+
+    describe "#compact! moves file blocks one at a time from the end of the disk to the leftmost free space block until all free space is contiguous" do
+      describe 'without limits' do
+        it 'fully compacts the short example' do
+          fs = Day9::BlockFileSystem.new(input)
+          fs.compact!
+          _(fs.to_s).must_equal expected_output_5
+        end
+
+        it 'fully compacts the long example' do
+          fs = Day9::BlockFileSystem.new(nil, diskmap: overall_scenario_input)
+          expected = '0099811188827773336446555566..............'
+          fs.compact!
+          _(fs.to_s).must_equal expected
+        end
+      end
+
+      describe "with limit 1 takes only one increment" do
+        it 'moves from initial input to state 1' do
+          fs = Day9::BlockFileSystem.new(input)
+          fs.compact!(limit: 1)
+          _(fs.to_s).must_equal expected_output_1
+        end
+
+        it 'moves from state 1 to state 2' do
+          fs = Day9::BlockFileSystem.new(expected_output_1)
+          fs.compact!(limit: 1)
+          _(fs.to_s).must_equal expected_output_2
+        end
+
+        it 'moves from state 2 to state 3' do
+          fs = Day9::BlockFileSystem.new(expected_output_2)
+          fs.compact!(limit: 1)
+          _(fs.to_s).must_equal expected_output_3
+        end
+
+        it 'moves from state 3 to state 4' do
+          fs = Day9::BlockFileSystem.new(expected_output_3)
+          fs.compact!(limit: 1)
+          _(fs.to_s).must_equal expected_output_4
+        end
+
+        it 'moves from state 4 to state 5' do
+          fs = Day9::BlockFileSystem.new(expected_output_4)
+          fs.compact!(limit: 1)
+          _(fs.to_s).must_equal expected_output_5
+        end
       end
     end
   end
